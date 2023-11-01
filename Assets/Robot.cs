4cfb775f5148323f6b42712181a46e47a86731ef
Muhammad Ashhad <betadrone596@gmail.com>
2023-10-31 20:26:45 -0400

    Chapter_10 Robot fire and ball anim

--- Assets/Robot.cs
+++ Assets/Robot.cs
@@ -18,15 +18,37 @@ public class Robot : MonoBehaviour
     private float timeLastFired;
 
     private bool isDead;
+
+    public Animator robot;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isDead = false;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isDead)
+        {
+            return;
+        }
+
+        transform.LookAt(player);
+
+        agent.SetDestination(player.position);
+
+        if(Vector3.Distance(transform.position, player.position) < range && Time.time - timeLastFired > fireRate)
+        {
+            timeLastFired = Time.time;
+            fire();
+        }
+    }
+
+    private void fire()
+    {
+        robot.Play("Fire");
     }
 }
