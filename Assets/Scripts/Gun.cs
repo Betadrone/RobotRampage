bdd11a821912877b7ae9c275b3570d522dc0036c
Muhammad Ashhad <betadrone596@gmail.com>
2023-11-03 16:23:46 -0400

    Chapter_10 Gun Damage programmed

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -20,12 +20,34 @@ public class Gun : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        zoomFOV = Constants.CameraDefaultZoom / zoomFactor;
         lastFireTime = Time.time - 10;
     }
 
     protected virtual void Update()
     {
+        // Right Click (Zoom)
+        if (Input.GetMouseButton(1))
+        {
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, zoomFOV, zoomSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Camera.main.fieldOfView = Constants.CameraDefaultZoom;
+        }
+    }
 
+    private void processHit(GameObject hitObject)
+    {
+        if (hitObject.GetComponent<Player>() != null)
+        {
+            hitObject.GetComponent<Player>().TakeDamage(damage);
+        }
+
+        if (hitObject.GetComponent<Robot>() != null)
+        {
+            hitObject.GetComponent<Robot>().TakeDamage(damage);
+        }
     }
 
     protected void Fire()
@@ -40,5 +62,12 @@ public class Gun : MonoBehaviour
             GetComponent<AudioSource>().PlayOneShot(dryFire);
         }
         GetComponentInChildren<Animator>().Play("Fire");
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, range)) 
+        {
+            processHit(hit.collider.gameObject);
+        }
     }
 }
